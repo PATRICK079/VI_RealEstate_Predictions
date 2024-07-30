ad113ca5a65af8dd18f7a4aa85e2ba0be30e0acd
Patrick Edosoma <157173680+PATRICK079@users.noreply.github.com>
2024-07-30 17:57:34 +0300

    Update web.py

--- web.py
+++ web.py
@@ -6,19 +6,20 @@ import pandas as pd
 # Load the trained model from file
 @st.cache_resource(show_spinner="Loading model...")
 def load_model():
-    model = joblib.load("/Users/sot/final_house_model.pkl")
+    model = joblib.load("/Users/sot/Documents/deployment/final_house_model.joblib")
     return  model
 # Load the preprocessor (e.g., column transformer, scaler, etc.) from file
 @st.cache_resource(show_spinner="Loading converter...")
 def load_converter():
-    converter = joblib.load("/Users/sot/final_converter.pkl")
+    converter = joblib.load("/Users/sot/Documents/deployment/final_converter.joblib")
     return converter
 # Load the list of column names used for feature engineering
+
 @st.cache_resource(show_spinner="Loading column names...")
 def load_col_names():
-    col_names = joblib.load("/Users/sot/poly_columes.pkl")
+    col_names = joblib.load('/Users/sot/Documents/deployment/poly_columns.joblib')
     return col_names
-# Make predictions using the model and input data
+
 @st.cache_data(show_spinner="Predicting...")
 def make_prediction(_model, _converter, _col_names, house_type, bedrooms, bathrooms, toilets, parking_spaces):
     # Prepare categorical data
@@ -31,11 +32,11 @@ def make_prediction(_model, _converter, _col_names, house_type, bedrooms, bathro
 
     # Combine numerical and encoded categorical data
     combined_df = pd.concat([num_df, cat_encoded], axis=1)
-    combined_df = combined_df.reindex(columns=col_names, fill_value=0)
+    combined_df = combined_df.reindex(columns=_col_names, fill_value=0)
 
     # Transform data and make prediction
-    transformed_data = converter.transform(combined_df.values)
-    prediction = model.predict(transformed_data)
+    transformed_data = _converter.transform(combined_df.values)
+    prediction = _model.predict(transformed_data)
     rounded_prediction = round(prediction[0])
 
     return f'The estimated rent price for the selected house type is around {rounded_prediction} Naira'
